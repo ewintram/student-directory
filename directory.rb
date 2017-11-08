e2a84224d8d2723f7a9c43a2efc8cb01b431f39a
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-08 20:54:01 +0000

    Add center method

--- directory.rb
+++ directory.rb
@@ -1,38 +1,26 @@
 def print_header
-  puts "The students of Twin Peaks Academy"
-  puts "--------------"
+  puts "The students of Twin Peaks Academy".center(50)
+  puts "--------------".center(50)
 end
 
 def print_array(students)
   students.each do |student|
-      puts "#{student[:name]}"
-      puts "Favourite hobby: #{student[:hobby]}"
-      puts "Height: #{student[:height]}"
-      puts "Country of birth: #{student[:birth_country]}"
-      puts "(#{student[:cohort]} cohort)"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students."
+  puts "Overall, we have #{students.count} great students.".center(50)
 end
 
 def input_students
-  puts "To finish, just hit return twice."
   puts "Please enter the name of the student."
+  puts "To finish, just hit return twice."
   name = gets.chomp
   students = []
   while !name.empty? do
-    puts "Please enter the favourite hobby of the student."
-    hobby = gets.chomp
-    puts "Please enter the height of the student in m."
-    height = gets.chomp
-    puts "Please enter the country of birth of the student."
-    birth_country = gets.chomp
-    students << {name: name, hobby: hobby, height: height, birth_country: birth_country, cohort: :november}
+    students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    puts "---"
-    puts "Please enter the name of the student."
     name = gets.chomp
   end
   students
